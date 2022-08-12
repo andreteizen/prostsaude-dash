cc6c131c123420defed4aaf620a98a4e7e03eae5
Andre Teizen <andre.teizen@gmail.com>
2022-08-12 15:02:24 -0300

    Ajustes

--- next.config.js
+++ next.config.js
@@ -2,4 +2,9 @@ module.exports = {
     env: {
       NEXT_PUBLIC_RAPIDAPI_KEY: "d055ae9f53mshad222a6e7e1d479p18e957jsnbc8978a84c49",
     },
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
 }
