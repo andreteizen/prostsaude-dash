0e4d740faba8b719b10edc4d7e06cee5163021f4
Andre Teizen <andre.teizen@gmail.com>
2022-08-24 13:59:03 -0300

    Teste produção CORS

--- pages/api/delete/[_id].js
+++ pages/api/delete/[_id].js
@@ -4,7 +4,13 @@ import connect from "../../../utils/database"
 
 export default async function handler (req,res){
     const { _id } = req.query;
-
+    
+    await NextCors(req, res, {
+        // Options
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        origin: '*',
+        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+     });
 
     if (req.method == 'DELETE'){
         const { db } = await connect();
